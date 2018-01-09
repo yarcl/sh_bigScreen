5e3d9fdf0cb2bb0fe98d6f90e2c86ef08df2fbe6
xiaozhi <zhi.xiao@wbkit.com>
2018-01-09 18:25:32 +0800

    for simple restful api demo

--- src/main/java/com/xqkj/action/LoginAction.java
+++ src/main/java/com/xqkj/action/LoginAction.java
@@ -1,43 +1,47 @@
 package com.xqkj.action;
 
-import com.xqkj.bean.RazorProduct;
+
+import com.alibaba.fastjson.JSONObject;
 import com.xqkj.bean.RazorUser;
-import com.xqkj.service.RazorProductService;
+
 import com.xqkj.service.UserService;
-import org.apache.shiro.web.session.HttpServletSession;
+
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.SessionAttributes;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
-import java.util.List;
 
 @Controller
 @RequestMapping("/")
-@SessionAttributes("user")
 public class LoginAction{
 
     @Autowired
     private UserService userService;
 
-    @RequestMapping("login.do")
-    public ModelAndView login(String username, String password, ModelAndView mav){
+    @RequestMapping(value="login.do", method = RequestMethod.POST, produces = {"application/json;charset=utf-8"})
+    public @ResponseBody Object login(String username, String password, HttpServletRequest request){
+        JSONObject obj = new JSONObject();
         try{
             RazorUser user = userService.login(username,password);
+
             if(user!=null){
                 user.setLoginPwd("");
-                mav.addObject("user", user);
-                mav.setViewName("index");
+                request.getSession().setAttribute("user", user);
+                obj.put("flag", true);
             } else {
-                mav.setViewName("login");
+                obj.put("flag", false);
             }
         } catch(Exception e){
             e.printStackTrace();
         }
-        return mav;
+        return obj;
     }
     @RequestMapping("/loginOut.do")
     public ModelAndView loginOut(ModelAndView mav, HttpServletRequest request){
